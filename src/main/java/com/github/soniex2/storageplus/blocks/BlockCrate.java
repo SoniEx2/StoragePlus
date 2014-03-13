d906e8ffc3b8ff2b428d03fb5d362a522ed8f805
SoniEx2 <EnderMoneyMod@gmail.com>
2014-03-13 14:20:55 -0300

    Crate textures
    TODO: connected textures

--- src/main/java/com/github/soniex2/storageplus/blocks/BlockCrate.java
+++ src/main/java/com/github/soniex2/storageplus/blocks/BlockCrate.java
@@ -3,13 +3,19 @@ package com.github.soniex2.storageplus.blocks;
 import com.github.soniex2.storageplus.StoragePlus;
 import com.github.soniex2.storageplus.tileentities.TileEntityCrate;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
 // TODO implement this
 public class BlockCrate extends BlockContainer {
+
+	private final IIcon[] icons = new IIcon[6];
 
 	public BlockCrate() {
 		super(Material.wood);
@@ -26,4 +32,23 @@ public class BlockCrate extends BlockContainer {
 		return new TileEntityCrate();
 	}
 
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerBlockIcons(IIconRegister register) {
+		icons[0] = register.registerIcon("storageplus:crate_oak");
+		icons[1] = register.registerIcon("storageplus:crate_spruce");
+		icons[2] = register.registerIcon("storageplus:crate_brich");
+		icons[3] = register.registerIcon("storageplus:crate_jungle");
+		icons[4] = register.registerIcon("storageplus:crate_acacia");
+		icons[5] = register.registerIcon("storageplus:crate_darkoak");
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public IIcon getIcon(int side, int metadata) {
+		if (metadata >= icons.length || metadata < 0) {
+			return icons[0];
+		}
+		return icons[metadata];
+	}
 }
